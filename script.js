de58c4d5fa92a4ae006623340f1e713585d831fe
amrxt <amrxt11@outlook.com>
2022-12-09 21:04:43 +0530

    Complete getComputerChoice() with strings as output

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function getComputerChoice(){
+    const x = Math.round((Math.random()*2))+1;
+    if(x === 1){
+        return "Rock";
+    }
+    else if(x === 2){
+        return "Paper";
+    }
+    else{
+        return "Scissors";
+    }
+}
