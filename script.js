6c7a9f23d1d10ef2f139b346ed34b87e28e08ddb
amrxt <amrxt11@outlook.com>
2022-12-09 21:47:49 +0530

    Use boolean to keep track of score

--- script.js
+++ script.js
@@ -15,33 +15,41 @@ function getComputerChoice(){
 function playTheGame(playerSelection, computerSelection){
     if(playerSelection === computerSelection){
         console.log("It's a Tie.");
+        return 0;
     }
     else if(playerSelection === "Rock"){
         if(computerSelection === "Paper"){
             console.log("You lose! Paper beats Rock.");
+            return true;
         }
         else if( computerSelection === "Scissors"){
             console.log("You win! Rock beats Scissors.");
+            return false;
         }
     }
     else if(playerSelection === "Paper"){
         if(computerSelection === "Rock"){
             console.log("You win! Paper beats Rock.");
+            return false;
         }
         else if(computerSelection === "Scissors"){
             console.log("You lose! Scissors cut Paper.");
+            return true;
         }
     }
     else if(playerSelection === "Scissors"){
         if(computerSelection === "Paper"){
             console.log("You win! Scissors cut Paper.");
+            return false;
         }
         else if(computerSelection === "Rock"){
             console.log("You lose! Rock beats Scissors.");
+            return true;
         }
     }
     else{
         console.log("Something went wrong. We will be back(or not).");
+        return 0;
     }
 }
 
@@ -53,12 +61,29 @@ function caseInSensitive(str){
 
 let playerSelection = "";
 let computerSelection = "";
+let computerWins = false;
+let playerScore = 0;
+let compScore = 0;
 
-function game(playerSelection, computerSelection){
+function game(){
     console.log("This is a game of Rock, Paper and Scissors.");
     for(let i = 0; i<5 ; i++){
         playerSelection = caseInSensitive(prompt("Please Enter your choice."));
         computerSelection = getComputerChoice();
-        playTheGame(playerSelection, computerSelection);
+        computerWins = playTheGame(playerSelection, computerSelection);
+        if(computerWins === 0){
+            console.log("You : "+playerScore);
+            console.log("The A.I. : "+compScore);
+            continue;
+        }
+        else if(computerWins){
+            compScore++;
+        }
+        else{
+            playerScore++;
+        }
+        console.log("You : "+playerScore);
+        console.log("The A.I. : "+compScore);
     }
+    
 }
